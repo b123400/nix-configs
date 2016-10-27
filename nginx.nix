47d2f18933815a8d7affa1ced912c29d1cbe2407
Brian Chan <b123400@gmail.com>
2016-10-27 16:09:57 +0700

    Good bye butai

--- nginx.nix
+++ nginx.nix
@@ -100,31 +100,11 @@ in
         return 301 https://b123400.net;
     }
 
-    # server {
-    # #    listen       5000;
-    #     listen       but.ai;
-    #     listen 443 ssl;
-    # #    listen       www.but.ai;
-    #     server_name  but.ai www.but.ai;
-
-    #     include /etc/nginx/conf.d/ssl-wellknown.conf;
-    #     ssl_certificate /etc/letsencrypt/live/but.ai/fullchain.pem;
-    #     ssl_certificate_key /etc/letsencrypt/live/but.ai/privkey.pem;
-
-    #     location / {
-    #       proxy_set_header X-Real-IP $remote_addr;
-    #       proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-    #       proxy_set_header Host $http_host;
-    #       proxy_set_header X-NginX-Proxy true;
-
-    #       proxy_set_header Upgrade $http_upgrade;
-    #       proxy_set_header Connection "upgrade";
-
-    #       proxy_http_version 1.1;
-    #       proxy_pass http://127.0.0.1:5000/;
-    #       proxy_redirect off;
-    #     }
-    # }
+    server {
+         listen       80;
+         server_name  but.ai www.but.ai;
+         return 301 https://blog.b123400.net/butai-shutdown;
+    }
   '';
 }
 
