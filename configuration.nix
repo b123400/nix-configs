a2a0e63094ca9e684c6c3ad634ea46bbe89cb503
Brian Chan <b123400@gmail.com>
2017-01-07 02:01:59 +0800

    Update network interface for kvm

--- configuration.nix
+++ configuration.nix
@@ -34,10 +34,10 @@
   networking.firewall = {
     enable = true;
     extraCommands = ''
-      iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
+      iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o enp0s4 -j MASQUERADE
     '';
     extraStopCommands = ''
-      iptables -t nat -D POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
+      iptables -t nat -D POSTROUTING -s 10.8.0.0/24 -o enp0s4 -j MASQUERADE
     '';
     allowedUDPPorts = [ 1194 ];
     allowedTCPPorts = [ 9091 ];
