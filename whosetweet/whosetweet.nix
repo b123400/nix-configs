9db781f1092feca8f80b5f18b1e559de412d8efd
Brian Chan <i@b123400.net>
2018-04-15 14:49:36 +0900

    Upgrade to nixos 18.03

--- whosetweet/whosetweet.nix
+++ whosetweet/whosetweet.nix
@@ -6,7 +6,7 @@ with (import <nixpkgs> {});
   stack }:
 
 let
-  ghc = haskell.compiler.ghc7102;
+  ghc = haskell.compiler.ghc7103;
 in
 haskell.lib.buildStackProject {
   name = "whosetweet";
@@ -14,8 +14,8 @@ haskell.lib.buildStackProject {
   ghc = ghc;
   src = fetchgit {
     url = git://github.com/b123400/whosetweet;
-    rev = "76649f2e06210b988abc77d7c329c0b45755c3fa";
-    sha256 = "0ypxkba6rqbdn6ajhwcnhjqh46n84dqz0pxilbg6hnldy6k8wdw2";
+    rev = "30c00d09ce31d07b5828f818dc20fecf82c7c33b";
+    sha256 = "16wahwfg13cynqhfv6j8l4b0lyncqsrr638dm349vwwxhxzhr79y";
   };
 
   configurePhase = ''
@@ -24,6 +24,7 @@ haskell.lib.buildStackProject {
     mkdir -p fake-home
     export STACK_ROOT=$NIX_BUILD_TOP/.stack
     stack config set system-ghc --global true
+    stack config set install-ghc --global false
   '';
 
   installPhase = ''
