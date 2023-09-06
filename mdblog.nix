f24a64e6f37ea383839c94d5cae2c7b06ae90278
b123400 <i@b123400.net>
2023-09-06 22:58:35 +0900

    Update mdblog

--- mdblog.nix
+++ mdblog.nix
@@ -5,8 +5,8 @@ in pkgs.rustPlatform.buildRustPackage rec {
 
       src = pkgs.fetchgit {
         url = "https://git.sr.ht/~not/mdblog";
-        rev = "d0612d2a6c0fd4d736108359a024b50bcc3b3ce4";
-        sha256 = "sha256-Y2L3nitIV6EuHWP5KTbe/OccL5eGeRSSlZorKWiVGpM=";
+        rev = "81e6f7e3f9976dff128bf94ca9356e8e46a8469b";
+        sha256 = "sha256-BKziTBfrFyVErDMSnLU8ueLDPkbyZWW1WJvDjR0yXAg=";
       };
 
       cargoSha256 = "sha256-TOADPVf6G9E2bQEJEsVifF0JdwDVCmeNzDvC+tZT9lY=";
